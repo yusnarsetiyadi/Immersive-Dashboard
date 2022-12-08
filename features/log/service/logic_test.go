94dda96b2e9f27cb29928fb3ccef120cf48a1fde
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2022-12-08 13:48:18 +0700

    mockery testing class&log

--- features/log/service/logic_test.go
+++ features/log/service/logic_test.go
@@ -12,7 +12,7 @@ import (
 func TestCreateLog(t *testing.T) {
 	repo := new(mocks.LogRepository)
 	t.Run("Success Create log", func(t *testing.T) {
-		inputRepo := log.Core{Title: "alta", Feedback: "good", Status: "unit1", MenteeID: 1}
+		inputRepo := log.Core{Title: "alta", Feedback: "alta", Status: "alta", UserID: 1, MenteeID: 1}
 		repo.On("CreateLog", inputRepo).Return(nil).Once()
 		srv := New(repo)
 		err := srv.CreateLog(inputRepo)
@@ -21,7 +21,7 @@ func TestCreateLog(t *testing.T) {
 	})
 
 	t.Run("Failed Create log", func(t *testing.T) {
-		inputRepo := log.Core{Title: "alta", Feedback: "good", Status: "unit1", MenteeID: 1}
+		inputRepo := log.Core{Title: "alta", Feedback: "alta", Status: "alta", UserID: 1, MenteeID: 1}
 		repo.On("CreateLog", inputRepo).Return(errors.New("failed")).Once()
 		srv := New(repo)
 		err := srv.CreateLog(inputRepo)
@@ -29,4 +29,78 @@ func TestCreateLog(t *testing.T) {
 		assert.Equal(t, "failed", err.Error())
 		repo.AssertExpectations(t)
 	})
+}
+
+func TestUpdateLog(t *testing.T) {
+	repo := new(mocks.LogRepository)
+	t.Run("Success update log", func(t *testing.T) {
+		inputRepo := log.Core{Title: "alta", Feedback: "alta", Status: "alta", UserID: 1, MenteeID: 1}
+		repo.On("UpdateLog", inputRepo, 1).Return(nil).Once()
+		srv := New(repo)
+		err := srv.UpdateLog(inputRepo, 1)
+		assert.NoError(t, err)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Failed Update log", func(t *testing.T) {
+		inputRepo := log.Core{Title: "alta", Feedback: "alta", Status: "alta", UserID: 1, MenteeID: 1}
+		repo.On("UpdateLog", inputRepo, 1).Return(errors.New("failed")).Once()
+		srv := New(repo)
+		err := srv.UpdateLog(inputRepo, 1)
+		assert.NotNil(t, err)
+		assert.Equal(t, "failed", err.Error())
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestDeleteLog(t *testing.T) {
+	repo := new(mocks.LogRepository)
+	t.Run("Success delete log", func(t *testing.T) {
+		repo.On("DeleteLog", 1).Return(nil).Once()
+		srv := New(repo)
+		err := srv.DeleteLog(1)
+		assert.NoError(t, err)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Failed delete log", func(t *testing.T) {
+		repo.On("DeleteLog", 1).Return(errors.New("failed")).Once()
+		srv := New(repo)
+		err := srv.DeleteLog(1)
+		assert.NotNil(t, err)
+		assert.Equal(t, "failed", err.Error())
+		repo.AssertExpectations(t)
+	})
+}
+
+func TestGetAllLog(t *testing.T) {
+	repo := new(mocks.LogRepository)
+	t.Run("Success get all log", func(t *testing.T) {
+		inputRepo := []log.Core{{ID: 1, Title: "alta", Feedback: "alta", Status: "alta", UserID: 1, MenteeID: 1}}
+		repo.On("GetAllLog").Return(inputRepo, nil).Once()
+		srv := New(repo)
+		response, err := srv.GetAllLog("")
+		assert.NoError(t, err)
+		assert.Equal(t, inputRepo[0].Title, response[0].Title)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("Failed get all log", func(t *testing.T) {
+		repo.On("GetAllLog").Return(nil, errors.New("failed")).Once()
+		srv := New(repo)
+		response, err := srv.GetAllLog("")
+		assert.NotNil(t, err)
+		assert.Nil(t, response)
+		repo.AssertExpectations(t)
+	})
+
+	// t.Run("Success get all class", func(t *testing.T) {
+	// 	inputRepo := []class.Core{{ID: 1, ClassName: "alta", UserID: 1}}
+	// 	repo.On("GetAllClass").Return(inputRepo, nil).Once()
+	// 	srv := New(repo)
+	// 	response, err := srv.GetAllClass("query")
+	// 	assert.NoError(t, err)
+	// 	assert.Equal(t, inputRepo[0].ClassName, response[0].ClassName)
+	// 	repo.AssertExpectations(t)
+	// })
 }
